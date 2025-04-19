a5417855c0460e94b8c4866aa629528f1bbf8d44
waseelamansoor <waseelamansoor445@gmail.com>
2025-04-19 18:14:35 +0500

    add files

--- welcome.php
+++ welcome.php
@@ -1,7 +1,12 @@
-<?php
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css">
+ 
+ <?php
  session_start();
- 
- session_destroy();
- header("location:login.php");
- 
+ if(!isset($_SESSION['user'])){
+     header('location:login.php');
+ }
  ?>
+ 
+ <h1>Welcome <?php echo $_SESSION['user']?></h1>
+ 
+ <a href="logout.php" class="btn btn-danger">Logout</a>
